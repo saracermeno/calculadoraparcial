1b0aff9fa5f364730463ea17f04a73edca4b944a
saracermeno <101078943+saracermeno@users.noreply.github.com>
2022-05-02 19:32:45 -0600

    Add files via upload

---
+++ Ejercicio12.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    cout << "Lista de numeros del 20 al 30" << endl;
+    for (int j = 20; j < 31; j++){
+        cout << j << " ";
+    }
+    system("pause");
+    return 0;
+}
